0eee4aaaeb41357c58b71271831fcec8d609a2d4
Eduardo García <eduardo.gl24@hotmail.com>
2022-07-03 18:40:46 -0500

    header and header responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  routeName: string = '';
+  event$
+ 
+  constructor(private location: Location) {
+    this.event$ = location.onUrlChange((val) => {
+      this.routeName = val.replace("/", "");
+    })
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
