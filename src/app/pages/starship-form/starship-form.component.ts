ca0bd0916bb89b52640155cd3dd37d3044892aae
Eduardo García <eduardo.gl24@hotmail.com>
2022-07-07 05:47:43 -0500

    fix error localstorage

--- src/app/pages/starship-form/starship-form.component.ts
+++ src/app/pages/starship-form/starship-form.component.ts
@@ -108,6 +108,9 @@ export class StarshipFormComponent implements OnInit, OnChanges {
         this.starshipsService.addStarship(this.starshipForm.value);
         this.buttonDelete = true;
       }
+    } else {
+      this.starshipsService.addStarship(this.starshipForm.value);
+      this.buttonDelete = true;
     }
   }
 
